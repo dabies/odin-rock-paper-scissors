9f93a381c66faebdc72dd0d92f25784c9f1c1248
Daniel <dabies11@gmail.com>
2024-06-13 13:50:04 -0700

    Added input buttons and basic display messages
    Added three buttons for user input, rock paper and scissors. These use
    a click event listener. Next step is to improve the user interface. As
    of now the only interface is a scoreboard.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -13,6 +13,7 @@ function getComputerChoice() {
     return choice;
 }
 
+/* this function was made to get user input via an alert message which is now obsolete
 function getHumanChoice() {
     let hChoice = prompt("Do you choose Rock, Paper or Scissors?");
     hChoice = hChoice.toLowerCase();
@@ -27,21 +28,27 @@ function getHumanChoice() {
             alert('Invalid answer.');
             return console.log('Invalid answer.');
     }
-}
+} */
+const displayDiv = document.querySelector('.display');
+const btnDiv = document.querySelector('.buttons');
+btnDiv.addEventListener('click', event => {
+    let hChoice = event.target.className;
+    let comp = getComputerChoice();
+    playRound(comp, hChoice);
+    displayDiv.textContent = `Human: ${humanScore} Machine: ${computerScore}`;
+
+});
 
 function playRound(computerChoice, humanChoice) {
     if (computerChoice === 'rock') {
         switch(humanChoice){
             case 'rock':
-                alert('Draw. You think like a computer.');
-                return console.log("It's a draw.");
+                return console.log("It's a draw. You think like a computer.");
             case 'paper':
                 humanScore++;
-                alert('You won that round!');
                 return console.log('You win! Paper beats Rock.');
             case 'scissors':
                 computerScore++;
-                alert('You lost that round.');
                 return console.log('You lose. Scissors loses to Rock.');
         }
 
@@ -49,45 +56,39 @@ function playRound(computerChoice, humanChoice) {
         switch(humanChoice){
             case 'rock':
                 computerScore++;
-                alert('You lost that round.');
                 return console.log('You lose. Rock is defeated by Paper.');
             case 'paper':
-                alert('Draw. You think like a computer.');
                 return console.log("It's a draw.");
             case 'scissors':
                 humanScore++;
-                alert('You won that round!');
                 return console.log('You win! Scissors beats Paper.');
         }
     } else {
         switch(humanChoice){
             case 'rock':
                 humanScore++;
-                alert('You won this round!');
                 return console.log('You Win! Rock beats Scissors.');
             case 'paper':
                 computerScore++;
-                alert('You lost that round.');
                 return console.log('You lose. Paper is defeated by Scissors.');
             case 'scissors':
-                alert('Draw. You think like a computer.');
                 return console.log("It's a draw.");
         }
     }
 }
 
 function playGame() {
-    while(humanScore < 3 && computerScore < 3) {
-        let human = getHumanChoice();
+    while(humanScore < 5 && computerScore < 5) {
+        let human = hChoice;
         let comp = getComputerChoice();
         playRound(comp, human);
     }
 }
 
-playGame();
-if (humanScore > computerScore) {
-    console.log(`Congratulations! You defeated the computer by a score of ${humanScore} to ${computerScore}.`)
-} else {
-    console.log(`You were defeated by our computer overlords by a score of ${humanScore} to ${computerScore}.`)
+if (humanScore == 5 || computerScore == 5) {
+    if (humanScore > computerScore) {
+        console.log(`Congratulations! You defeated the computer by a score of ${humanScore} to ${computerScore}.`)
+    } else {
+        console.log(`You were defeated by our computer overlords by a score of ${humanScore} to ${computerScore}.`)
+    }
 }
-
