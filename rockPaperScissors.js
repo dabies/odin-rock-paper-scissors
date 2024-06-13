c79b2bb83acedec385558b54f2a47646ea4c8092
Daniel <dabies11@gmail.com>
2024-06-13 14:39:09 -0700

    Finished User Interface, added some CSS
    Finished scoreboard, as well as interactive elements such as changing h1
    to announce winner. Also added some CSS to html to center page, add
    background color and make buttons look better. All from a flight from
    Merida to Guadalajara Mexico. Flying home for an important 3 game series
    against Puebla.

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -29,66 +29,76 @@ function getHumanChoice() {
             return console.log('Invalid answer.');
     }
 } */
-const displayDiv = document.querySelector('.display');
+const header = document.querySelector('h1');
+const resultPara = document.querySelector('.result');
+const scorePara = document.querySelector('.score');
+scorePara.textContent = `Human: ${humanScore} Machine: ${computerScore}`;
 const btnDiv = document.querySelector('.buttons');
 btnDiv.addEventListener('click', event => {
     let hChoice = event.target.className;
     let comp = getComputerChoice();
     playRound(comp, hChoice);
-    displayDiv.textContent = `Human: ${humanScore} Machine: ${computerScore}`;
-
+    scorePara.textContent = `Human: ${humanScore} Machine: ${computerScore}`;
+    announceWinner();
 });
 
 function playRound(computerChoice, humanChoice) {
     if (computerChoice === 'rock') {
         switch(humanChoice){
             case 'rock':
-                return console.log("It's a draw. You think like a computer.");
+                return resultPara.textContent = ("It's a draw. You think like a computer.");
             case 'paper':
                 humanScore++;
-                return console.log('You win! Paper beats Rock.');
+                return resultPara.textContent = ('You win! Paper beats Rock.');
             case 'scissors':
                 computerScore++;
-                return console.log('You lose. Scissors loses to Rock.');
+                return resultPara.textContent = ('You lose. Scissors loses to Rock.');
         }
 
     } else if (computerChoice === 'paper') {
         switch(humanChoice){
             case 'rock':
                 computerScore++;
-                return console.log('You lose. Rock is defeated by Paper.');
+                return resultPara.textContent = ('You lose. Rock is defeated by Paper.');
             case 'paper':
-                return console.log("It's a draw.");
+                return resultPara.textContent = ("It's a draw. You think like a computer.");
             case 'scissors':
                 humanScore++;
-                return console.log('You win! Scissors beats Paper.');
+                return resultPara.textContent = ('You win! Scissors beats Paper.');
         }
     } else {
         switch(humanChoice){
             case 'rock':
                 humanScore++;
-                return console.log('You Win! Rock beats Scissors.');
+                return resultPara.textContent = ('You Win! Rock beats Scissors.');
             case 'paper':
                 computerScore++;
-                return console.log('You lose. Paper is defeated by Scissors.');
+                return resultPara.textContent = ('You lose. Paper is defeated by Scissors.');
             case 'scissors':
-                return console.log("It's a draw.");
+                return resultPara.textContent = ("It's a draw. You just scissored a computer.");
         }
     }
 }
 
+/* now defunct function from console version
 function playGame() {
     while(humanScore < 5 && computerScore < 5) {
         let human = hChoice;
         let comp = getComputerChoice();
         playRound(comp, human);
     }
-}
+}*/
 
-if (humanScore == 5 || computerScore == 5) {
-    if (humanScore > computerScore) {
-        console.log(`Congratulations! You defeated the computer by a score of ${humanScore} to ${computerScore}.`)
-    } else {
-        console.log(`You were defeated by our computer overlords by a score of ${humanScore} to ${computerScore}.`)
+function announceWinner() {
+    if (humanScore == 5 || computerScore == 5) {
+        if (humanScore > computerScore) {
+            header.style.backgroundColor = 'green';
+            return header.textContent = (`Congratulations! You defeated the computer by a 
+            score of ${humanScore} to ${computerScore}. Looks like humanity stands a chance.`);
+        } else {
+            header.style.backgroundColor = 'red';
+            return header.textContent = (`You were defeated by our computer overlords by a 
+            score of ${humanScore} to ${computerScore}. Humanity is doomed.`);
+        }
     }
 }
